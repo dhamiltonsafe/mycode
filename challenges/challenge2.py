db47416357c24bab734f86439f9d01a5058342a3
dhamiltonsafe <david.hamilton@safecu.org>
2026-05-19 19:37:19 +0000

    A bit on error handling

---
+++ challenges/challenge2.py
@@ -0,0 +1,132 @@
+classinfo = {
+    "all": [
+        {
+            "name": "Aaron",
+            "skill level": "wondrous",
+            "spirit animal": "Alpaca",
+            "super power": "Structure Weakening",
+        },
+        {
+            "name": "Ahamed",
+            "skill level": "admirable",
+            "spirit animal": "Shark",
+            "super power": "Super Strength",
+        },
+        {
+            "name": "Alice",
+            "skill level": "amazing",
+            "spirit animal": "Goat",
+            "super power": "Weather Control",
+        },
+        {
+            "name": "Angela",
+            "skill level": "astonishing",
+            "spirit animal": "Banana",
+            "super power": "Flight",
+        },
+        {
+            "name": "Ayrat",
+            "skill level": "awesome",
+            "spirit animal": "Horse",
+            "super power": "X-ray Vision",
+        },
+        {
+            "name": "Brandon",
+            "skill level": "brilliant",
+            "spirit animal": "Eagle",
+            "super power": "Shape of: A Giant Shark",
+        },
+        {
+            "name": "Brian",
+            "skill level": "cool",
+            "spirit animal": "Rabbit",
+            "super power": "Invisibility",
+        },
+        {
+            "name": "Carl",
+            "skill level": "enjoyable",
+            "spirit animal": "Water buffalo",
+            "super power": "Transformation",
+        },
+        {
+            "name": "Chris",
+            "skill level": "excellent",
+            "spirit animal": "Chicken",
+            "super power": "Pyrokinesis",
+        },
+        {
+            "name": "Cliff",
+            "skill level": "fabulous",
+            "spirit animal": "Duck",
+            "super power": "Invulnerability",
+        },
+        {
+            "name": "David",
+            "skill level": "fantastic",
+            "spirit animal": "Goose",
+            "super power": "Explosive Shouts",
+        },
+        {
+            "name": "Devin",
+            "skill level": "great",
+            "spirit animal": "Pigeon",
+            "super power": "Matter Ingestion",
+        },
+        {
+            "name": "Felicia",
+            "skill level": "incredible",
+            "spirit animal": "Turkey",
+            "super power": "Zoolingualism",
+        },
+        {
+            "name": "Jason",
+            "skill level": "magnificent",
+            "spirit animal": "Aardvark",
+            "super power": "Height Manipulation",
+        },
+        {
+            "name": "JP",
+            "skill level": "marvelous",
+            "spirit animal": "Aardwolf",
+            "super power": "Hydrokinesis",
+        },
+        {
+            "name": "Lars",
+            "skill level": "outstanding",
+            "spirit animal": "Elephant",
+            "super power": "Needle Projection",
+        },
+        {
+            "name": "Linda",
+            "skill level": "phenomenal",
+            "spirit animal": "Leopard",
+            "super power": "Stretchy",
+        },
+        {
+            "name": "Micah",
+            "skill level": "pleasant",
+            "spirit animal": "Albatross",
+            "super power": "Steel Skin",
+        },
+        {
+            "name": "Namit",
+            "skill level": "pleasing",
+            "spirit animal": "Alligator",
+            "super power": "Teleportation",
+        },
+        {
+            "name": "Tania",
+            "skill level": "remarkable",
+            "spirit animal": "Lynx",
+            "super power": "Polyglot",
+        },
+        {
+            "name": "Zachary",
+            "skill level": "super",
+            "spirit animal": "Fox",
+            "super power": "Eat Anything",
+        },
+    ]
+}
+
+print 
---
+++ oop-intro/no-oop.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# standard library
+import random
+
+def main():
+    """Run time code"""
+
+    #create lists to store scores
+    player1_dice = []
+    player2_dice = []
+
+    for i in range(3):
+        player1_dice.append(random.randint(1,6))
+        player2_dice.append(random.randint(1,6))
+
+    print ("Player 1 rolled", player1_dice)
+    print ("Player 2 rolled", player2_dice)
+
+    if sum(player1_dice) == sum(player2_dice):
+        print("Draw")
+    elif sum(player1_dice) > sum(player2_dice):
+        print("Player 1 wins")
+    else:
+        print("Player 2 wins")
+
+if __name__ == "__main__":
+    main()
+
